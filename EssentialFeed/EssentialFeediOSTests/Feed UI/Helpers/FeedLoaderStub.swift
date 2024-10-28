9bcdee250afbf4c64ff1788eb4f7fa75a6a15665
Swift En Profundidad <freelancemerlos@gmail.com>
2024-10-28 17:50:42 +0100

    Remove duplicates files

---
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedLoaderStub.swift
@@ -0,0 +1,20 @@
+//
+//  FeedLoaderStub.swift
+//  EssentialFeedAppTests
+//
+//  Created by Juan Carlos merlos albarracin on 18/10/24.
+//
+
+import EssentialFeed
+
+class FeedLoaderStub: FeedLoader {
+  private let result: FeedLoader.Result
+  
+  init(result: FeedLoader.Result) {
+    self.result = result
+  }
+  
+  func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    completion(result)
+  }
+}
