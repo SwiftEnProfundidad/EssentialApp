e764e0bbe563c44baa7ae8522e8f57808155423a
Swift En Profundidad <freelancemerlos@gmail.com>
2024-10-28 17:06:15 +0100

    Move Feed UI composition details from the EssentialFeediOS to the EssentialFeedApp module (Composition Root)

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImagePresenter.swift
@@ -8,22 +8,22 @@
 import EssentialFeed
 import Foundation
 
-protocol FeedImageView {
+public protocol FeedImageView {
     associatedtype Image
 
     func display(_ model: FeedImageViewModel<Image>)
 }
 
-final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
     private let view: View
     private let imageTransformer: (Data) -> Image?
 
-    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+    public init(view: View, imageTransformer: @escaping (Data) -> Image?) {
         self.view = view
         self.imageTransformer = imageTransformer
     }
 
-    func didStartLoadingImageData(for model: FeedImage) {
+    public func didStartLoadingImageData(for model: FeedImage) {
         view.display(FeedImageViewModel(
             description: model.description,
             location: model.location,
@@ -35,7 +35,7 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
 
     private struct InvalidImageDataError: Error {}
 
-    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+    public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
         guard let image = imageTransformer(data) else {
             return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
         }
@@ -49,7 +49,7 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
         ))
     }
 
-    func didFinishLoadingImageData(with _: Error, for model: FeedImage) {
+    public func didFinishLoadingImageData(with _: Error, for model: FeedImage) {
         view.display(FeedImageViewModel(
             description: model.description,
             location: model.location,
