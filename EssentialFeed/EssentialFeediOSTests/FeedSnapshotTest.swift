04760946242739c72eae3edf50aaba95a5b58625
Swift En Profundidad <freelancemerlos@gmail.com>
2024-11-04 22:15:39 +0100

    Record feed with content snapshot

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTest.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTest.swift
@@ -6,7 +6,8 @@
 //
 
 import XCTest
-import EssentialFeediOS
+import EssentialFeed
+@testable import EssentialFeediOS
 
 final class FeedSnapshotTest: XCTestCase {
   
@@ -15,6 +16,13 @@ final class FeedSnapshotTest: XCTestCase {
     
     sut.display(emptyFeed())
     record(snapshot: sut.snapshot(), name: "EMPTY_FEED")
+  }
+  
+  func test_feedWithContent() {
+    let sut = makeSUT()
+    
+    sut.display(feedWithContent())
+    record(snapshot: sut.snapshot(), name: "FEED_WITH_CONTENT")
   }
   
   // MARK: - Helpers
@@ -30,6 +38,22 @@ final class FeedSnapshotTest: XCTestCase {
   private func emptyFeed() -> [FeedImageCellController] {
     return []
   }
+  
+  private func feedWithContent() -> [ImageStub] {
+    return [
+      ImageStub(
+        description: "The East Side Gallery is an open-air gallery in San Francisco, California. It consists of a series of murals painted directly on a 1,250-opening-pixel wide strip, viewed from a distance of 240 feet, that is located on Exchange Street in the East Side between 7th and 8th arrondissements of the city.",
+        location: "East Side Gallery\nSan Francisco",
+        image: UIImage.make(withColor: .red)
+      ),
+      ImageStub(
+        description: "Garth Pier is a Grade II listed structure in San               Francisco. The structure, which was named after the garage that             leads to the Pier, was built in 1906.",
+        location: "Garth Pier\nSan Francisco",
+        image: UIImage.make(withColor: .green)
+      )
+    ]
+  }
+  
   
   private func record(snapshot: UIImage, name: String, file: StaticString = #file, line: UInt = #line) {
     guard let snapshotData = snapshot.pngData() else {
@@ -51,6 +75,28 @@ final class FeedSnapshotTest: XCTestCase {
       XCTFail("Failed to record snapshot vith error: \(error)", file: file, line: line)
     }
   }
+  
+  
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+  let viewModel: FeedImageViewModel<UIImage>
+  weak var controller: FeedImageCellController?
+  
+  init(description: String?, location: String?, image: UIImage?) {
+    viewModel = FeedImageViewModel(
+      description: description,
+      location: location,
+      image: image,
+      isLoading: false,
+      shouldRetry: image == nil)
+  }
+  
+  func didRequestImage() {
+    controller?.display(viewModel)
+  }
+  
+  func didCancelImageRequest() { }
 }
 
 extension UIViewController {
@@ -59,5 +105,16 @@ extension UIViewController {
     return renderer.image { action in
       view.layer.render(in: action.cgContext)
     }
+  }
+}
+
+private extension FeedViewController {
+  func display(_ stubs: [ImageStub]) {
+    let cells: [FeedImageCellController] = stubs.map { stub in
+      let cellController = FeedImageCellController(delegate: stub)
+      stub.controller = cellController
+      return cellController
+    }
+    display(cells)
   }
 }
