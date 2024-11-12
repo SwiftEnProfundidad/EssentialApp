dba3a6d6b1f01a875b7bb099421e0816b91a7f36
Swift En Profundidad <freelancemerlos@gmail.com>
2024-11-12 08:00:28 +0100

    Fix potential bug when cancelling requests in UITableView didEndDisplayingCell method - This method is invoked after calling `reloadData`, so we'd be cancelling requests in the wrong models or crash in case the model has less items than the previous model

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -9,84 +9,92 @@ import EssentialFeed
 import UIKit
 
 public protocol FeedViewControllerDelegate {
-    func didRequestFeedRefresh()
+  func didRequestFeedRefresh()
 }
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
-    @IBOutlet public private(set) var errorView: ErrorView?
-    private var viewAppeared = false
+  @IBOutlet public private(set) var errorView: ErrorView?
 
-    public var delegate: FeedViewControllerDelegate?
+  private var loadingController = [IndexPath: FeedImageCellController]()
+  private var viewAppeared = false
 
-    private var tableModel = [FeedImageCellController]() {
-        didSet { tableView.reloadData() }
+  public var delegate: FeedViewControllerDelegate?
+
+  private var tableModel = [FeedImageCellController]() {
+    didSet { tableView.reloadData() }
+  }
+
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override public func viewIsAppearing(_ animated: Bool) {
+    super.viewIsAppearing(animated)
+
+    if !viewAppeared {
+      refresh()
+      viewAppeared = true
     }
+  }
 
-    override public func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override public func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-
-        if !viewAppeared {
-            refresh()
-            viewAppeared = true
-        }
-    }
-  
   public override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
-    
+
     tableView.sizeHeaderToFit()
   }
 
-    @IBAction func refresh() {
-        delegate?.didRequestFeedRefresh()
-    }
-    public func display(_ viewModel: FeedLoadingViewModel) {
-        refreshControl?.update(isRefreshing: viewModel.isLoading)
-    }
+  @IBAction func refresh() {
+    delegate?.didRequestFeedRefresh()
+  }
 
-    public func display(_ cellControllers: [FeedImageCellController]) {
-        tableModel = cellControllers
-    }
+  public func display(_ viewModel: FeedLoadingViewModel) {
+    refreshControl?.update(isRefreshing: viewModel.isLoading)
+  }
 
-    public func display(_ viewModel: FeedErrorViewModel) {
-        if let message = viewModel.errorMessage {
-            errorView?.show(message: message)
-        } else {
-            errorView?.hideMessage()
-        }
-    }
+  public func display(_ cellControllers: [FeedImageCellController]) {
+    loadingController = [:]
+    tableModel = cellControllers
+  }
 
-    override public func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
-        tableModel.count
+  public func display(_ viewModel: FeedErrorViewModel) {
+    if let message = viewModel.errorMessage {
+      errorView?.show(message: message)
+    } else {
+      errorView?.hideMessage()
     }
+  }
 
-    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        cellController(forRowAt: indexPath).view(in: tableView)
-    }
+  override public func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+    tableModel.count
+  }
 
-    override public func tableView(_: UITableView, didEndDisplaying _: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cancelCellControllerLoad(forRowAt: indexPath)
-    }
+  override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    cellController(forRowAt: indexPath).view(in: tableView)
+  }
 
-    public func tableView(_: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-        for indexPath in indexPaths {
-            cellController(forRowAt: indexPath).preload()
-        }
-    }
+  override public func tableView(_: UITableView, didEndDisplaying _: UITableViewCell, forRowAt indexPath: IndexPath) {
+    guard tableModel.count > indexPath.row else { return }
+    cancelCellControllerLoad(forRowAt: indexPath)
+  }
 
-    public func tableView(_: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
-        indexPaths.forEach(cancelCellControllerLoad)
+  public func tableView(_: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+    for indexPath in indexPaths {
+      cellController(forRowAt: indexPath).preload()
     }
+  }
 
-    private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
-        tableModel[indexPath.row]
-    }
+  public func tableView(_: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+    indexPaths.forEach(cancelCellControllerLoad)
+  }
 
-    private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
-        cellController(forRowAt: indexPath).cancelLoad()
-    }
+  private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
+    let controller = tableModel[indexPath.row]
+    loadingController[indexPath] = controller
+    return controller
+  }
+
+  private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
+    cellController(forRowAt: indexPath).cancelLoad()
+    loadingController[indexPath] = nil
+  }
 }
