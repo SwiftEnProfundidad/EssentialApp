ec6fc958ebfb6c40ddfa8de49dc373287b464807
Swift En Profundidad <freelancemerlos@gmail.com>
2024-11-13 05:50:15 +0100

    Remove unnecessary Main storyboard - the initial set up happens in code

--- EssentialFeedApp/EssentialFeedAppTest/SceneDelegateTest.swift
+++ EssentialFeedApp/EssentialFeedAppTest/SceneDelegateTest.swift
@@ -5,8 +5,9 @@
 //  Created by Juan Carlos merlos albarracin on 28/10/24.
 //
 
-import XCTest
 import EssentialFeediOS
+import XCTest
+
 @testable import EssentialFeedApp
 
 final class SceneDelegateTest: XCTestCase {
@@ -14,12 +15,27 @@ final class SceneDelegateTest: XCTestCase {
   func test_sceneWillConnectToSession_configurationRootViewController() {
     let sut = SceneDelegate()
     sut.configureWindow(whith: UIWindow())
-    
+
     let root = sut.window?.rootViewController
     let rootNavigation = root as? UINavigationController
     let topViewController = rootNavigation?.topViewController
-    
+
     XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
-    XCTAssertTrue(topViewController is FeedViewController, "Expected a feed view controller as top view controller, got \(String(describing: topViewController)) instead")
+    XCTAssertTrue(
+      topViewController is FeedViewController, "Expected a feed view controller as top view controller, got \(String(describing: topViewController)) instead")
   }
+
+  func test_configureWindow_setWindowAsKeyAndVisible() {
+    let window = UIWindow()
+    window.windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+
+    let sut = SceneDelegate()
+    sut.window = window
+
+    sut.configureWindow(whith: window)
+
+    XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
+    XCTAssertFalse(window.isHidden, "Expected window to be visible")
+  }
+
 }
