17a4ef9267e7830b1f6252b490b8dbb876df8e31
Swift En Profundidad <freelancemerlos@gmail.com>
2024-11-14 00:17:26 +0100

    Replace FeedLoader composition with Combine operators

---
+++ EssentialFeedApp/EssentialFeedApp/CombineHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  CombineHelpers.swift
+//  EssentialFeedApp
+//
+//  Created by Juan Carlos merlos albarracin on 14/11/24.
+//
+
+import Foundation
+import Combine
+import EssentialFeed
+
+
--- EssentialFeedApp/EssentialFeedApp/MainQueueDispatchDecorator.swift
+++ EssentialFeedApp/EssentialFeedApp/MainQueueDispatchDecorator.swift
@@ -5,43 +5,33 @@
 //  Created by Juan Carlos merlos albarracin on 30/9/24.
 //
 
-import Foundation
 import EssentialFeed
 import EssentialFeediOS
+import Foundation
 
 final class MainQueueDispatchDecorator<T> {
-    private let decoratee: T
+  private let decoratee: T
 
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
+  init(decoratee: T) {
+    self.decoratee = decoratee
+  }
 
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async {
-                completion()
-            }
-        }
+  func dispatch(completion: @escaping () -> Void) {
+    guard Thread.isMainThread else {
+      return DispatchQueue.main.async {
         completion()
+      }
     }
-}
-
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch {
-                completion(result)
-            }
-        }
-    }
+    completion()
+  }
 }
 
 extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch {
-                completion(result)
-            }
-        }
+  func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+    decoratee.loadImageData(from: url) { [weak self] result in
+      self?.dispatch {
+        completion(result)
+      }
     }
+  }
 }
